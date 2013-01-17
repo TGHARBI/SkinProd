1d9bf5b171cc58a75b42cca49e9f6f359c5a8e09
TGHARBI <tijani.gharbi@gmail.com>
2013-01-17 21:12:47 +0100

    Start Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkinProd::Application.config.secret_token = 'a8fee73862d7d318e649e252245088dfb74e750683d90c99479c5b4c231e79f55ca329d0b5b797acb2f7fc35678716b264c7dd870f1002379c7f22aa2984a9b1'
